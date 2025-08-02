555ac6d0080b9189a354eb3043743a95419dacd4
Alex Grazela <agrazela@gmail.com>
2025-08-02 19:44:13 -0400

    Add project files.

---
+++ Simple3dEditor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Simple3dEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
